cf2ff882e6e38b0d01b80135ab42e6baf774f5d2
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-31 20:42:18 +0200

    Initial Commit

---
+++ SwiftUIButtonStyles/SwiftUIButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonStylesApp.swift
+//  SwiftUIButtonStyles
+//
+//  Created by Ramill Ibragimov on 31.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
